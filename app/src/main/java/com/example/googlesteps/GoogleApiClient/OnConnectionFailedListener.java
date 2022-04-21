7d660dbb588e1f66ae037eaad05026c417612b46
Abhi1314 <abhijackson619@gmail.com>
2022-04-21 13:42:22 +0530

    initial commit

---
+++ app/src/main/java/com/example/googlesteps/GoogleApiClient/OnConnectionFailedListener.java
@@ -0,0 +1,4 @@
+package com.example.googlesteps.GoogleApiClient;
+
+public interface OnConnectionFailedListener {
+}
---
+++ app/src/main/java/com/example/googlesteps/View/OnClickListener.java
@@ -0,0 +1,4 @@
+package com.example.googlesteps.View;
+
+public interface OnClickListener {
+}
